535eeb61b29ec757ee54f85f7539fbe11ad02eac
Сырцев Вадим Игоревич <syrtcevvi@gmail.com>
2024-09-07 21:01:18 +0300

    Refactor `style` files

--- crates/teloxide-inline-widgets/src/types.rs
+++ crates/teloxide-inline-widgets/src/types.rs
@@ -1,12 +1,5 @@
-mod calendar_style;
-mod checkbox_list_style;
-mod common_style;
-mod radio_list_style;
+mod callback_query_data;
 mod size;
-mod widget_styles;
+mod styles;
 
-pub use self::{
-    calendar_style::CalendarStyle, checkbox_list_style::CheckboxListStyle,
-    common_style::CommonStyle, radio_list_style::RadioListStyle, size::Size,
-    widget_styles::WidgetStyles,
-};
+pub use self::{callback_query_data::CallbackQueryData, size::Size, styles::*};
---
+++ crates/teloxide-inline-widgets/src/types/styles.rs
@@ -0,0 +1,10 @@
+mod calendar_style;
+mod checkbox_list_style;
+mod common_style;
+mod radio_list_style;
+mod widget_styles;
+
+pub use self::{
+    calendar_style::*, checkbox_list_style::*, common_style::*, radio_list_style::*,
+    widget_styles::*,
+};
